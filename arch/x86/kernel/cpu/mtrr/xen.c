a9a016286749b3f406f7c43b66fd2634566c2054
Mark McLoughlin <markmc@redhat.com>
2008-01-29 17:43:25 +0000

    xen mtrr: Add xen_{get,set}_mtrr() implementations
    Straightforward apart from the hack to turn mtrr_ops->set()
    into a no-op on all but one CPU.
    
    [ Impact: complete Xen mtrr implementation ]
    
    Signed-off-by: Mark McLoughlin <markmc@redhat.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- arch/x86/kernel/cpu/mtrr/xen.c
+++ arch/x86/kernel/cpu/mtrr/xen.c
@@ -7,6 +7,52 @@
 #include <asm/xen/hypervisor.h>
 #include <asm/xen/hypercall.h>
 
+static void xen_set_mtrr(unsigned int reg, unsigned long base,
+			 unsigned long size, mtrr_type type)
+{
+	struct xen_platform_op op;
+	int error;
+
+	/* mtrr_ops->set() is called once per CPU,
+	 * but Xen's ops apply to all CPUs.
+	 */
+	if (smp_processor_id())
+		return;
+
+	if (size == 0) {
+		op.cmd = XENPF_del_memtype;
+		op.u.del_memtype.handle = 0;
+		op.u.del_memtype.reg    = reg;
+	} else {
+		op.cmd = XENPF_add_memtype;
+		op.u.add_memtype.mfn     = base;
+		op.u.add_memtype.nr_mfns = size;
+		op.u.add_memtype.type    = type;
+	}
+
+	error = HYPERVISOR_dom0_op(&op);
+	BUG_ON(error != 0);
+}
+
+static void xen_get_mtrr(unsigned int reg, unsigned long *base,
+			 unsigned long *size, mtrr_type *type)
+{
+	struct xen_platform_op op;
+
+	op.cmd = XENPF_read_memtype;
+	op.u.read_memtype.reg = reg;
+	if (HYPERVISOR_dom0_op(&op) != 0) {
+		*base = 0;
+		*size = 0;
+		*type = 0;
+		return;
+	}
+
+	*size = op.u.read_memtype.nr_mfns;
+	*base = op.u.read_memtype.mfn;
+	*type = op.u.read_memtype.type;
+}
+
 static int __init xen_num_var_ranges(void)
 {
 	int ranges;
@@ -29,7 +75,10 @@ static int __init xen_num_var_ranges(void)
 static struct mtrr_ops xen_mtrr_ops = {
 	.vendor            = X86_VENDOR_UNKNOWN,
 	.get_free_region   = generic_get_free_region,
+	.set               = xen_set_mtrr,
+	.get               = xen_get_mtrr,
 	.have_wrcomb       = positive_have_wrcomb,
+	.validate_add_page = generic_validate_add_page,
 	.use_intel_if	   = 0,
 	.num_var_ranges	   = xen_num_var_ranges,
 };
