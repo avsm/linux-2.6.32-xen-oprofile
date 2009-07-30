63fc31d9102c9dd2af4d8a31e8b5679f0c3d2483
Gianluca Guida <gianluca.guida@citrix.com>
2009-07-30 22:54:36 +0100

    Xen balloon: fix totalram_pages counting.
    Change totalram_pages when a single page is added/removed to the
    ballooned list. This avoid totalram_pages to be set erroneously to
    max_pfn at boot.
    
    Signed-off-by: Gianluca Guida <gianluca.guida@citrix.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- drivers/xen/balloon.c
+++ drivers/xen/balloon.c
@@ -138,6 +138,8 @@ static void balloon_append(struct page *page)
 		list_add(&page->lru, &ballooned_pages);
 		balloon_stats.balloon_low++;
 	}
+
+	totalram_pages--;
 }
 
 /* balloon_retrieve: rescue a page from the balloon, if it is not empty. */
@@ -157,6 +159,8 @@ static struct page *balloon_retrieve(void)
 	}
 	else
 		balloon_stats.balloon_low--;
+
+	totalram_pages++;
 
 	return page;
 }
@@ -249,7 +253,6 @@ static int increase_reservation(unsigned long nr_pages)
 	}
 
 	balloon_stats.current_pages += rc;
-	totalram_pages = balloon_stats.current_pages;
 
  out:
 	spin_unlock_irqrestore(&balloon_lock, flags);
@@ -312,7 +315,6 @@ static int decrease_reservation(unsigned long nr_pages)
 	BUG_ON(ret != nr_pages);
 
 	balloon_stats.current_pages -= nr_pages;
-	totalram_pages = balloon_stats.current_pages;
 
 	spin_unlock_irqrestore(&balloon_lock, flags);
 
@@ -410,7 +412,6 @@ static int __init balloon_init(void)
 	pr_info("xen_balloon: Initialising balloon driver.\n");
 
 	balloon_stats.current_pages = min(xen_start_info->nr_pages, max_pfn);
-	totalram_pages   = balloon_stats.current_pages;
 	balloon_stats.target_pages  = balloon_stats.current_pages;
 	balloon_stats.balloon_low   = 0;
 	balloon_stats.balloon_high  = 0;
