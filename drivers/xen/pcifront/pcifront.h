511a4949ee0992cc286a58043b382b894f6183c1
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2009-11-05 16:33:27 -0500

    xen-pcifront: Remove ia64 from pcifront.c support.
    The ia64 support of PCI frontend requires changes in the arch/ia64/pci/pci.c
    which are currently not present. Will revisit once those are present.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- drivers/xen/pcifront/pcifront.h
+++ drivers/xen/pcifront/pcifront.h
@@ -40,8 +40,6 @@ struct pcifront_device {
 
 };
 
-#ifndef __ia64__
-
 struct pcifront_sd {
 	int domain;
 	struct pcifront_device *pdev;
@@ -61,37 +59,10 @@ static inline void pcifront_init_sd(struct pcifront_sd *sd,
 	sd->pdev = pdev;
 }
 
-
 static inline void pcifront_setup_root_resources(struct pci_bus *bus,
 						 struct pcifront_sd *sd)
 {
 }
-
-#else /* __ia64__ */
-
-#include <linux/acpi.h>
-#include <asm/pci.h>
-#define pcifront_sd pci_controller
-
-extern void xen_add_resource(struct pci_controller *, unsigned int,
-			     unsigned int, struct acpi_resource *);
-extern void xen_pcibios_setup_root_windows(struct pci_bus *,
-					   struct pci_controller *);
-
-static inline struct pcifront_device *
-pcifront_get_pdev(struct pcifront_sd *sd)
-{
-	return (struct pcifront_device *)sd->platform_data;
-}
-
-static inline void pcifront_setup_root_resources(struct pci_bus *bus,
-						 struct pcifront_sd *sd)
-{
-	xen_pcibios_setup_root_windows(bus, sd);
-}
-
-#endif /* __ia64__ */
-
 extern struct rw_semaphore pci_bus_sem;
 
 int pcifront_scan_root(struct pcifront_device *pdev,
