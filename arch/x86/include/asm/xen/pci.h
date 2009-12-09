60f59ecb361b68aacbda50d24b840a8d1255ab2f
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2009-12-09 15:17:59 -0500

    xen-pci: Provide functionality to associate a PCI device with a domain.
    .. and also to find the domain id based on the PCI device and
    to unregister a PCI device from a domain. This mechanism will be
    employed by the pciback and Xen events channel to associate
    the PCI device with the correct domain.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- arch/x86/include/asm/xen/pci.h
+++ arch/x86/include/asm/xen/pci.h
@@ -7,6 +7,11 @@ int xen_create_msi_irq(struct pci_dev *dev,
 			struct msi_desc *msidesc,
 			int type);
 int xen_destroy_irq(int irq);
+
+int xen_find_device_domain_owner(struct pci_dev *dev);
+int xen_register_device_domain_owner(struct pci_dev *dev, uint16_t domain);
+int xen_unregister_device_domain_owner(struct pci_dev *dev);
+
 #else
 static inline int xen_register_gsi(u32 gsi, int triggering, int polarity)
 {
@@ -22,6 +27,19 @@ static inline int xen_create_msi_irq(struct pci_dev *dev,
 static inline int xen_destroy_irq(int irq)
 {
 	return -1;
+}
+static inline int xen_find_device_domain_owner(struct pci_dev *dev)
+{
+	return -1;
+}
+static inline int xen_register_device_domain_owner(struct pci_dev *dev,
+						   uint16_t domain)
+{
+ 	return -1;
+}
+static inline int xen_unregister_device_domain_owner(struct pci_dev *dev)
+{
+ 	return -1;
 }
 #endif
 
