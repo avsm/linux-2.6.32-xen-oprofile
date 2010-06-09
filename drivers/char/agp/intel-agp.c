a2395df466453e0bf7ee223c449a29dd3b0ed7f0
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2010-06-09 10:49:46 -0400

    intel-agp: Warn when !USE_PCI_DMA_API and inserting bogus bus addresses.
    The i915 and newer have code that utilizes the PCI DMA API. But
    that code is enabled only if CONFIG_DMAR is enabled. So provide
    an error to user if the kernel hasn't been compiled with this
    and fixup the bus address.
    
    We MUST revisit this in the future.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/char/agp/intel-agp.c
+++ drivers/char/agp/intel-agp.c
@@ -9,6 +9,8 @@
 #include <linux/pagemap.h>
 #include <linux/agp_backend.h>
 #include "agp.h"
+#include <xen/page.h>
+#include <asm/xen/page.h>
 
 /*
  * If we have Intel graphics, we're not going to have anything other than
@@ -298,8 +300,20 @@ static void intel_agp_insert_sg_entries(struct agp_memory *mem,
 	int i, j;
 
 	for (i = 0, j = pg_start; i < mem->page_count; i++, j++) {
+		phys_addr_t phys = page_to_phys(mem->pages[i]);
+		if (xen_pv_domain()) {
+			phys_addr_t xen_phys = PFN_PHYS(pfn_to_mfn(
+					page_to_pfn(mem->pages[i])));
+			if (xen_phys != phys) {
+				printk(KERN_ERR "Compile kernel with " \
+					"CONFIG_DMAR to get rid of this " \
+					"warning!\n");
+				WARN_ON_ONCE(xen_phys != phys);
+				/* Fixup: */
+				phys = xen_phys;
+			}
 		writel(agp_bridge->driver->mask_memory(agp_bridge,
-				page_to_phys(mem->pages[i]), mask_type),
+				phys, mask_type),
 		       intel_private.gtt+j);
 	}
 
