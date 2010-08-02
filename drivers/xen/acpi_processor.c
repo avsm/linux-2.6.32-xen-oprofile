f399e7d18ad3128388e228665653f7a85c6ae2ec
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2010-08-02 16:37:43 -0700

    xen/acpi: rename reserved -> access_size
    acpi_power_register's "reserved" became "access_size"
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- drivers/xen/acpi_processor.c
+++ drivers/xen/acpi_processor.c
@@ -227,7 +227,7 @@ static int xen_cx_notifier(struct acpi_processor *pr, int action)
 		data->reg.space_id = reg->space_id;
 		data->reg.bit_width = reg->bit_width;
 		data->reg.bit_offset = reg->bit_offset;
-		data->reg.access_size = reg->reserved;
+		data->reg.access_size = reg->access_size;
 		data->reg.address = reg->address;
 
 		/* Get dependency relationships, _CSD is not supported yet */
