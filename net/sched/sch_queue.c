a6f1c28b7504bdb312d3f02401ae4385028fa6cb
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2010-08-18 17:40:50 -0700

    net/sch_queue: fix up checkpatch issues
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- net/sched/sch_queue.c
+++ net/sched/sch_queue.c
@@ -30,15 +30,16 @@
 #define TCQ_CHECKPOINT 0
 #define TCQ_DEQUEUE    1
 
-struct queue_sched_data
-{
-	/* this packet is the first packet which should not be delivered.
-	 * If it is NULL, queue_enqueue will set it to the next packet it sees. */
+struct queue_sched_data {
+	/*
+	 * This packet is the first packet which should not be
+	 * delivered.  If it is NULL, queue_enqueue will set it to the
+	 * next packet it sees.
+	 */
 	struct sk_buff *stop;
 };
 
-struct tc_queue_qopt
-{
+struct tc_queue_qopt {
 	/* 0: reset stop packet pointer
 	 * 1: dequeue to stop pointer */
 	int action;
@@ -51,123 +52,105 @@ static int skb_remove_foreign_references(struct sk_buff *skb)
 
 static int queue_enqueue(struct sk_buff *skb, struct Qdisc* sch)
 {
-  struct queue_sched_data *q = qdisc_priv(sch);
+	struct queue_sched_data *q = qdisc_priv(sch);
 
-  if (likely(sch->qstats.backlog + skb->len <= FIFO_BUF))
-  {
-    if (!q->stop)
-      q->stop = skb;
+	if (likely(sch->qstats.backlog + skb->len <= FIFO_BUF)) {
+		if (!q->stop)
+			q->stop = skb;
 
-    if (!skb_remove_foreign_references(skb)) {
-      printk("error removing foreign ref\n");
-      return qdisc_reshape_fail(skb, sch);
-    }
+		if (!skb_remove_foreign_references(skb)) {
+			printk(KERN_DEBUG "error removing foreign ref\n");
+			return qdisc_reshape_fail(skb, sch);
+		}
 
-    return qdisc_enqueue_tail(skb, sch);
-  }
-  printk("queue reported full: %d,%d\n", sch->qstats.backlog, skb->len);
+		return qdisc_enqueue_tail(skb, sch);
+	}
+	printk(KERN_WARNING "queue reported full: %d,%d\n",
+	       sch->qstats.backlog, skb->len);
 
-  return qdisc_reshape_fail(skb, sch);
+	return qdisc_reshape_fail(skb, sch);
 }
 
 /* dequeue doesn't actually dequeue until the release command is
  * received. */
 static struct sk_buff *queue_dequeue(struct Qdisc* sch)
 {
-  struct queue_sched_data *q = qdisc_priv(sch);
-  struct sk_buff* peek;
-  /*
-  struct timeval tv;
+	struct queue_sched_data *q = qdisc_priv(sch);
+	struct sk_buff *peek;
 
-  if (!q->stop) {
-    do_gettimeofday(&tv);
-    printk("packet dequeued at %lu.%06lu\n", tv.tv_sec, tv.tv_usec);
-  }
-  */
+	if (sch->flags & TCQ_F_THROTTLED)
+		return NULL;
 
-  if (sch->flags & TCQ_F_THROTTLED)
-    return NULL;
+	peek = (struct sk_buff *)((sch->q).next);
 
-  peek = (struct sk_buff *)((sch->q).next);
+	/* this pointer comparison may be shady */
+	if (peek == q->stop) {
+		/*
+		 * This is the tail of the last round. Release it and
+		 * block the queue
+		 */
+		sch->flags |= TCQ_F_THROTTLED;
+		return NULL;
+	}
 
-  /* this pointer comparison may be shady */
-  if (peek == q->stop) {
-    /*
-    do_gettimeofday(&tv);
-    printk("stop packet at %lu.%06lu\n", tv.tv_sec, tv.tv_usec);
-    */
-
-    /* this is the tail of the last round. Release it and block the queue */
-    sch->flags |= TCQ_F_THROTTLED;
-    return NULL;
-  }
-
-  return qdisc_dequeue_head(sch);
+	return qdisc_dequeue_head(sch);
 }
 
 static int queue_init(struct Qdisc *sch, struct nlattr *opt)
 {
-  sch->flags |= TCQ_F_THROTTLED;
+	sch->flags |= TCQ_F_THROTTLED;
 
-  return 0;
+	return 0;
 }
 
-/* receives two messages:
+/*
+ * receives two messages:
  *   0: checkpoint queue (set stop to next packet)
- *   1: dequeue until stop */
-static int queue_change(struct Qdisc* sch, struct nlattr* opt)
+ *   1: dequeue until stop
+ */
+static int queue_change(struct Qdisc *sch, struct nlattr *opt)
 {
-  struct queue_sched_data *q = qdisc_priv(sch);
-  struct tc_queue_qopt* msg;
-  /*
-  struct timeval tv;
-  */
+	struct queue_sched_data *q = qdisc_priv(sch);
+	struct tc_queue_qopt *msg;
 
-  if (!opt || nla_len(opt) < sizeof(*msg))
-    return -EINVAL;
+	if (!opt || nla_len(opt) < sizeof(*msg))
+		return -EINVAL;
 
-  msg = nla_data(opt);
+	msg = nla_data(opt);
 
-  if (msg->action == TCQ_CHECKPOINT) {
-    /* reset stop */
-    q->stop = NULL;
-  } else if (msg->action == TCQ_DEQUEUE) {
-    /* dequeue */
-    sch->flags &= ~TCQ_F_THROTTLED;
-    netif_schedule_queue(sch->dev_queue);
-    /*
-    do_gettimeofday(&tv);
-    printk("queue release at %lu.%06lu (%d bytes)\n", tv.tv_sec, tv.tv_usec,
-          sch->qstats.backlog);
-    */
-  } else {
-    return -EINVAL;
-  }
+	if (msg->action == TCQ_CHECKPOINT) {
+		/* reset stop */
+		q->stop = NULL;
+	} else if (msg->action == TCQ_DEQUEUE) {
+		/* dequeue */
+		sch->flags &= ~TCQ_F_THROTTLED;
+		netif_schedule_queue(sch->dev_queue);
+	} else {
+		return -EINVAL;
+	}
 
-  return 0;
+	return 0;
 }
 
 struct Qdisc_ops queue_qdisc_ops = {
-  .id          =       "queue",
-  .priv_size   =       sizeof(struct queue_sched_data),
-  .enqueue     =       queue_enqueue,
-  .dequeue     =       queue_dequeue,
-  .peek        =       qdisc_peek_head,
-  .init        =       queue_init,
-  .change      =       queue_change,
-  .owner       =       THIS_MODULE,
+	.id          =       "queue",
+	.priv_size   =       sizeof(struct queue_sched_data),
+	.enqueue     =       queue_enqueue,
+	.dequeue     =       queue_dequeue,
+	.peek        =       qdisc_peek_head,
+	.init        =       queue_init,
+	.change      =       queue_change,
+	.owner       =       THIS_MODULE,
 };
 
 static int __init queue_module_init(void)
 {
-  printk("loading queue\n");
-  return register_qdisc(&queue_qdisc_ops);
+	return register_qdisc(&queue_qdisc_ops);
 }
 
 static void __exit queue_module_exit(void)
 {
-  printk("queue unloaded\n");
-  unregister_qdisc(&queue_qdisc_ops);
+	unregister_qdisc(&queue_qdisc_ops);
 }
 module_init(queue_module_init)
 module_exit(queue_module_exit)
