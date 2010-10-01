050d7e389148d9babf5b63f4802c6f0a0c3249fe
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2010-10-01 09:53:57 -0700

    x86: fix build problems with pgt lock
    virt_to_page() ends up needing quite a lot of definitions in scope
    when NUMA and/or hotplug memory are enabled.  Including them all
    in a core header to allow inlining is impractical, so just pull
    the functions out-of-line in .c files.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -14,8 +14,6 @@
 	 : (prot))
 
 #ifndef __ASSEMBLY__
-#include <linux/mm_types.h>
-
 /*
  * ZERO_PAGE is a global shared page that is always zero: used
  * for zero-mapped memory areas etc..
@@ -26,16 +24,7 @@ extern unsigned long empty_zero_page[PAGE_SIZE / sizeof(unsigned long)];
 extern spinlock_t pgd_lock;
 extern struct list_head pgd_list;
 
-static inline void pgd_set_mm(pgd_t *pgd, struct mm_struct *mm)
-{
-	BUILD_BUG_ON(sizeof(virt_to_page(pgd)->index) < sizeof(mm));
-	virt_to_page(pgd)->index = (pgoff_t)mm;
-}
-
-static inline struct mm_struct *pgd_page_get_mm(struct page *page)
-{
-	return (struct mm_struct *)page->index;
-}
+extern struct mm_struct *pgd_page_get_mm(struct page *page);
 
 #ifdef CONFIG_PARAVIRT
 #include <asm/paravirt.h>
