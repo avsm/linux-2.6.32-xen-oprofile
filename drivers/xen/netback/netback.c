08bfda7932f9bb94f2c558bf979533ea743646c0
Paul Durrant <paul.durrant@citrix.com>
2010-12-14 20:35:21 +0000

    xen: netback: Add a missing test to tx_work_todo.
    Adda test so that, when netback is using worker threads, net_tx_action()
    gets called in a timely manner when the pending_inuse list is populated.
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- drivers/xen/netback/netback.c
+++ drivers/xen/netback/netback.c
@@ -1716,6 +1716,10 @@ static inline int tx_work_todo(struct xen_netbk *netbk)
 	if (netbk->dealloc_cons != netbk->dealloc_prod)
 		return 1;
 
+	if (netbk_copy_skb_mode == NETBK_DELAYED_COPY_SKB &&
+	    !list_empty(&netbk->pending_inuse_head))
+		return 1;
+
 	if (((nr_pending_reqs(netbk) + MAX_SKB_FRAGS) < MAX_PENDING_REQS) &&
 			!list_empty(&netbk->net_schedule_list))
 		return 1;
